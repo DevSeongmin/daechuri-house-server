4ee56437a9a3071fb1cc2e6438e43e50595739dc
DevSeongmin <hsm990423@gmail.com>
2026-01-12 15:51:54 +0900

    INIT

---
+++ src/main/java/org/daechurihouse/DaechuriHouseApplication.java
@@ -0,0 +1,13 @@
+package org.daechurihouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaechuriHouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaechuriHouseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/daechurihouse/DaechuriHouseApplicationTests.java
@@ -0,0 +1,13 @@
+package org.daechurihouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaechuriHouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
